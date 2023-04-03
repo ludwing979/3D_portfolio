c5abf6dbf770d0674e62710fc3f861e4d8b27fda
ludwing979 <magoludwing@gmail.com>
2023-04-03 14:56:27 -0600

    Adding the correct information of my profile

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,11 +9,11 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
+import scrum from "./tech/scrum.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import infosys from "./company/infosys.png";
+import sintel from "./company/sintel.png";
+import cnh from "./company/cnh.png";
+import tata from "./company/tata.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -45,18 +45,18 @@ export {
   git,
   html,
   javascript,
-  mongodb,
+  scrum,
   nodejs,
   reactjs,
   redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  sintel,
+  cnh,
+  tata,
   carrent,
+  infosys,
   jobit,
   tripguide,
 };
